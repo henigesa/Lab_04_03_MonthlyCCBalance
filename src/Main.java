e1e3220e784c26c9e9b58198a6804c4301b4fbc7
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-05 22:47:04 -0500

    with code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+//import scanner
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+//A program that starts with a credit card balance of $5000 and then calculates the interest at a rate
+//of 17%.  Assume the user does not make any payments.  Display the interest due after one month
+//and again after two months.
+        //declare variables
+        int ccBalance ;
+        double ccInterestOneMonth ;
+        double ccInterestTwoMonth ;
+        //prompt user to enter their credit card balance
+        System.out.println("Please enter your credit card balance:");
+        //take the user input
+        ccBalance = input.nextInt() ;
+        //calculate next two months of interest
+        ccInterestOneMonth = ccBalance * 0.17 ;
+        ccInterestTwoMonth = ccInterestOneMonth * 2 ;
+        //output the calculated interests to the user
+        System.out.println("Your interest due after one month is $" + ccInterestOneMonth ) ;
+        System.out.println("Your interest due after two months is $" + ccInterestTwoMonth ) ;
     }
 }
